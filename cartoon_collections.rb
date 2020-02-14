03d91705a3431a73b13cf7dbf048817e1af5cffa
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-14 08:00:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,8 @@
 def roll_call_dwarves(names)# code an argument here
   # Your code here
-  names.each_with_index(1)
+  names.each_with_index do |name, i|
+    
+  end
 end
 
 def summon_captain_planet# code an argument here
