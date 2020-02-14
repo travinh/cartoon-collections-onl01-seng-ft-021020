cf48dd4f09ce9807f3560a9bb860f85c55d95581
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-14 08:24:23 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names)# code an argument here
   # Your code here
+  names.each_with_index do |name, i|
+    puts "#{i+1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(names)# code an argument here
   # Your code here
+  # we could use map/collect
+  # names.map {|name| name.capitalize}
+  names.collect do |name|
+    name.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(words)# code an argument here
   # Your code here
+  words.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(string)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  i=0 
+  
+  while i<string.length
+    string2 = cheese_types.find {|item| string.include?(item)}
+    i+=1
+  end
+  string2
 end
